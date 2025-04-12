cb2be32768ab4f3abf70c058339bb9cdcc9df4da
PokerGod-tom <zihewang56@gmail.com>
2025-04-12 16:42:30 +0000

    .

---
+++ app/levies/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
+        <h1>Levies</h1>
+        <p>This page will show strata levy information.</p>
+      </main>
+    );
+  }
+  
