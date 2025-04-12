881b4d8f1ef5abd49c59aedf9dfeebf6c5950574
PokerGod-tom <zihewang56@gmail.com>
2025-04-12 16:59:49 +0000

    hello

---
+++ app/committee/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
+        <h1>committee
+        <p>This page will show strata committee information </p>
+      </main>
+    );
+  }
+  
